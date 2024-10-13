f45712485558ecf66a8027bcdf99df88dcd51243
Haris <harisvengattiri@gmail.com>
2024-10-13 20:19:07 +0530

    fix: changed file as global variable

--- database.php
+++ database.php
@@ -1,18 +1,21 @@
 <?php
 require_once "config.php";
 
+$file = 'diary.txt';
+
 function add_diary() {
+    global $file;
 
     $heading = htmlspecialchars($_POST['heading']);
     $message = htmlspecialchars($_POST['message']);
     
     $formData = "Title: $heading\nMessage: $message\n\n";
-    $file = 'diary.txt';
     file_put_contents($file, $formData, FILE_APPEND | LOCK_EX);
 }
 
 function edit_diary() {
-    $file = 'diary.txt';
+    global $file;
+    
     $updating_file = htmlspecialchars($_POST['updating_file']);
     file_put_contents($file, $updating_file);
 }
