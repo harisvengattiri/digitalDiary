37b55e20d791a53e06e99b13f7ffc21f651b6fde
Haris <harisvengattiri@gmail.com>
2024-10-22 00:31:42 +0530

    fix: file and delimiter defined as constends and passed to function as parameters

--- database.php
+++ database.php
@@ -1,25 +1,26 @@
 <?php
 define('BASEURL', 'http://localhost/digitalDiary');
+define('DIARY_FILE', 'diary.txt');
+define('DELIMITER', '[next_page]');
 
-function saveDiary($current_page, $updated_file, $file = 'diary.txt', $delimiter = '[next_page]') {
+function saveDiary($current_page, $updated_file, $file = DIARY_FILE, $delimiter = DELIMITER) {
     $fileContents = file_get_contents($file);
     $pages = explode($delimiter, $fileContents);
     $updated_page = htmlspecialchars($updated_file);
 
     $page_index = $current_page-1;
-
     $pages[$page_index] = $updated_page;
     $new_file = implode($delimiter, $pages);
     file_put_contents($file, $new_file);
 }
 
-function nextPage($current_page, $file = 'diary.txt', $delimiter = '[next_page]') {
+function nextPage($current_page, $file = DIARY_FILE, $delimiter = DELIMITER) {
     checkNextPageExists($current_page,$file,$delimiter);
     $page = $current_page+1;
     return $page;
 }
 
-function prevPage($current_page, $file = 'diary.txt', $delimiter = '[next_page]') {
+function prevPage($current_page, $file = DIARY_FILE, $delimiter = DELIMITER) {
     checkPrevPageExists($current_page,$file,$delimiter);
     $page = $current_page-1;
     return $page;
