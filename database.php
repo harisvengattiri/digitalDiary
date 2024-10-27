1e15d1e0f78b78b6392b586190315ff9aaf1fb1e
Haris <harisvengattiri@gmail.com>
2024-10-27 17:09:27 +0530

    fix: removed unwanted constand arguments

--- database.php
+++ database.php
@@ -1,23 +1,21 @@
 <?php
-define('BASEURL', 'http://localhost/digitalDiary');
-define('DIARY_FILE', 'diary.txt');
-define('DELIMITER', '[next_page]');
+require_once "config.php";
 
 function saveDiary($updated_file) {
     
     $updated_page = htmlspecialchars($updated_file);
-    $processed_page = processPage($_POST['current_page'], $file = DIARY_FILE, $delimiter = DELIMITER);
+    $processed_page = processPage($_POST['current_page']);
     
     $pages = $processed_page['pages'];
     $updating_index = $processed_page['updating_index'];
     $pages[$updating_index] = $updated_page;
 
-    updateProcessedPage($pages, $file, $delimiter);
+    updateProcessedPage($pages);
 }
 
-function processPage($current_page, $file, $delimiter) {
-    $fileContents = file_get_contents($file);
-    $pages = explode($delimiter, $fileContents);
+function processPage($current_page) {
+    $fileContents = file_get_contents(DIARY_FILE);
+    $pages = explode(DELIMITER, $fileContents);
     $page_index = $current_page-1;
     return [
         'updating_index' => $page_index,
@@ -25,41 +23,41 @@ function processPage($current_page, $file, $delimiter) {
     ];
 }
 
-function updateProcessedPage($pages, $file, $delimiter) {
-    $new_file = implode($delimiter, $pages);
-    file_put_contents($file, $new_file);
+function updateProcessedPage($pages) {
+    $new_file = implode(DELIMITER, $pages);
+    file_put_contents(DIARY_FILE, $new_file);
 }
 
-function nextPage($current_page, $file = DIARY_FILE, $delimiter = DELIMITER) {
-    checkNextPageExists($current_page,$file,$delimiter);
+function nextPage($current_page) {
+    checkNextPageExists($current_page);
     $page = $current_page+1;
     return $page;
 }
 
-function prevPage($current_page, $file = DIARY_FILE, $delimiter = DELIMITER) {
-    checkPrevPageExists($current_page,$file,$delimiter);
+function prevPage($current_page) {
+    checkPrevPageExists($current_page);
     $page = $current_page-1;
     return $page;
 }
 
-function checkPrevPageExists($current_page,$file,$delimiter) {
-    $fileContents = file_get_contents($file);
-    $pages = explode($delimiter, $fileContents);
+function checkPrevPageExists($current_page) {
+    $fileContents = file_get_contents(DIARY_FILE);
+    $pages = explode(DELIMITER, $fileContents);
 
     if (!isset($pages[$current_page-2])) {
         throw new Exception();
     }  
 }
 
-function checkNextPageExists($current_page,$file,$delimiter) {
-    $fileContents = file_get_contents($file);
-    $pages = explode($delimiter, $fileContents);
+function checkNextPageExists($current_page) {
+    $fileContents = file_get_contents(DIARY_FILE);
+    $pages = explode(DELIMITER, $fileContents);
 
     if (!isset($pages[$current_page])) {
-        addDelimiter($file,$delimiter);
+        addDelimiter();
     }
 }
 
-function addDelimiter($file,$delimiter) {
-    file_put_contents($file, $delimiter, FILE_APPEND);
+function addDelimiter() {
+    file_put_contents(DIARY_FILE, DELIMITER, FILE_APPEND);
 }
