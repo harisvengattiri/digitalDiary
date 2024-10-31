5abd57f0d6284310facb05b1a5252ce756e6475d
Haris <harisvengattiri@gmail.com>
2024-11-01 01:15:51 +0530

    refactor: made diary page using its template file

--- index.php
+++ index.php
@@ -1,27 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/style.css">
-    <script src="assets/script.js"></script>
-    <title>Digital Diary</title>
-</head>
-<body>
-<h2>Digital Diary</h2>
 <?php
 require_once "diary.php";
 $current_page = getPageNumber();
 $page_data = getDiaryPageData($current_page);
-?>
-    <form action="controller.php" method="post">
-        <textarea id="editable-textarea" name="updating_file"><?php echo $page_data;?></textarea><br><br>
-        <input type="hidden" name="controller" value="diary">
-        <input type="hidden" name="current_page" value="<?php echo $current_page;?>">
 
-        <input type="submit" name="prev_page" value="prev">
-        <input type="submit" name="save_diary" value="save">
-        <input type="submit" name="next_page" value="next">
-    </form>
-</body>
-</html>
+$diary_template = file_get_contents("diary_template.html");
+
+$diary_template = str_replace("{{page_data}}", htmlspecialchars($page_data), $diary_template);
+$diary_template = str_replace("{{current_page}}", htmlspecialchars($current_page), $diary_template);
+
+echo $diary_template;
